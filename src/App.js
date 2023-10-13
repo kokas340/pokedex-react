202709471f7ae1d0feafb927b2279d45c42ca4aa
Jack Spinola <65230037+kokas340@users.noreply.github.com>
2023-10-13 18:25:22 +0200

    deeply

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Pokemon from './pages/Pokemon';
 import Contact from './pages/Contact';
 function App() {
   return (
-    <Router>
+    <Router basename='/pokedex-react' >
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
